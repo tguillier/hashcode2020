3e548c69cd187a867694b17404485e17662a9194
GUIDI Nicolas <nicolas.guidi@sogetrel.fr>
2020-02-20 18:20:17 +0100

    init commit degueu

---
+++ HashCode2020/Data/DataFormat.cs
@@ -0,0 +1,21 @@
+﻿namespace HashCode2020.Data
+{
+    public abstract class DataFormat<T>
+    {
+        string _rawData;
+        readonly string separator = " ";
+
+        public DataFormat(string rawData)
+        {
+            _rawData = rawData;
+        }
+
+        protected string GetDataFromLine(int line, int nb)
+        {
+            //todo
+            return null;
+        }
+
+        protected abstract T CreateFromRawData(string rawData);
+    }
+}
