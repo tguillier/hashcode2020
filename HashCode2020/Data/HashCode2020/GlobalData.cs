5aaf22d06b8d868d29808f622cc925cc9af8276e
GUIDI Nicolas <nicolas.guidi@sogetrel.fr>
2020-02-20 19:24:02 +0100

    classe globale

--- HashCode2020/Data/HashCode2020/GlobalData.cs
+++ HashCode2020/Data/HashCode2020/GlobalData.cs
@@ -7,7 +7,7 @@ namespace HashCode2020.Data.HashCode2020
     public class GlobalData
     {
         AllBooks AllBooks { get; set; }
-        Book Book { get; set; }
-        Library Library { get; set; }
+        List<Library> Libraries { get; set; }
+        int NbdaysToScan { get; set; }
     }
 }
