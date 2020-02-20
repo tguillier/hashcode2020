a537467e605e969defcda98816a92ebfdf67e09f
GUIDI Nicolas <nicolas.guidi@sogetrel.fr>
2020-02-20 19:22:57 +0100

    create globalData

--- HashCode2020/Data/HashCode2020/BookDataFormat.cs
+++ HashCode2020/Data/HashCode2020/BookDataFormat.cs
@@ -4,8 +4,15 @@ using System.Text;
 
 namespace HashCode2020.Data.HashCode2020
 {
-    public class BookDataFormat : DataFormat<Book>
+    public class BookDataFormat : DataFormat<GlobalData>
     {
+        public BookDataFormat(string rawData) : base(rawData)
+        {
+        }
 
+        protected override GlobalData CreateFromRawData(string rawData)
+        {
+            return null;
+        }
     }
 }
---
+++ HashCode2020/Data/HashCode2020/GlobalData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HashCode2020.Data.HashCode2020
+{
+    public class GlobalData
+    {
+        AllBooks AllBooks { get; set; }
+        Book Book { get; set; }
+        Library Library { get; set; }
+    }
+}
